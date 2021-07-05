e5b91563d3cf03bd87efad0b4f1af374ee305818
Bridget Turnbull <bridget@192-168-1-111.tpgi.com.au>
2021-07-05 11:12:32 +1000

    Update Z index to bring toast to front

--- ui/src/components/NotificationToast.js
+++ ui/src/components/NotificationToast.js
@@ -6,7 +6,7 @@ export default Notification = () => {
     const { notify, handleDismiss, notificationMessage } = useContext(UserContext);
 
     return (
-        <Toast style={{position: 'absolute', top: 10, right: 10}} show={notify} onClose={handleDismiss}>
+        <Toast style={{position: 'absolute', top: 10, right: 10, zIndex: 9999}} show={notify} onClose={handleDismiss}>
             <Toast.Header>
                 <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
                 <strong className="mr-auto">Account Verification</strong>
