ba6fb6fe6975b7370382c2a7df208847f2cfc550
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-13 15:20:59 +1100

    Styled up the index (Start) page as per design

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from '../components/Button';
 
 export default function Home() {
@@ -11,20 +12,33 @@ export default function Home() {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <main className="bg-gradient-to-tr from-primary-500 to-accent min-h-screen flex flex-col justify-center antialiased">
-        <div className="mx-auto max-w-md py-2 text-center space-y-10">
+        <div className="mx-auto max-w-md px-4 pt-8 pb-14 text-center space-y-6">
+          {/* Logo and divider */}
+          <div className="inline-block space-y-6">
+            <Image src="/logo-on-dark.svg" alt="Piper logo" width={79} height={102} />
+            <div className="border-b mix-blend-soft-light"></div>
+          </div>
+
+          {/* Product heading */}
           <h1 className="text-white text-3xl font-semibold tracking-tight">
             Start optimising <br />
             your savings
           </h1>
+
+          {/* Product short description */}
           <p className="text-white text-opacity-90 leading-relaxed">
             Piper helps you track and optimise your savings. For every dollar saved you get 10% cashback into your
             account.
           </p>
-          <Link href="/sign-in" passHref>
-            <Button as="a" variant="inverted" block>
-              Get started
-            </Button>
-          </Link>
+
+          {/* CTA to Account Verification flow */}
+          <div className="mx-auto w-56">
+            <Link href="/sign-in" passHref>
+              <Button as="a" variant="inverted" block>
+                Get started
+              </Button>
+            </Link>
+          </div>
         </div>
       </main>
     </div>
