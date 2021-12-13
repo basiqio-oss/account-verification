7ce46d8b30937aebe966fd5113b1a46b30ed40b2
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-14 10:31:56 +1100

    Added responsive heights on ProgressBar

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,7 +1,7 @@
 export function ProgressBar({ value }) {
   return (
     // TODO: add transition class to animate
-    <div className="relative h-2 w-full">
+    <div className="relative h-1 sm:h-1.5 md:h-2 w-full">
       <div
         style={{ width: `${value}%` }}
         className="absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-500 to-accent rounded-r"
