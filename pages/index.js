fe4c4cbe801fc7768be023dd343afe00cd31858f
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-13 12:07:39 +1100

    Added background gradient, Inter font, tweaked text styles

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,17 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Basiq account verification</title>
+        <title>Piper</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <main className="bg-brand-500 text-white min-h-screen flex flex-col justify-center">
-        <div className="mx-auto max-w-md py-2 text-center space-y-10 ">
-          <h1 className="text-6xl">Start optimising your savings</h1>
-          <p>
+      <main className="bg-gradient-to-tr from-primary-500 to-accent min-h-screen flex flex-col justify-center antialiased">
+        <div className="mx-auto max-w-md py-2 text-center space-y-10">
+          <h1 className="text-white text-3xl font-semibold tracking-tight">
+            Start optimising <br />
+            your savings
+          </h1>
+          <p className="text-white text-opacity-90 leading-relaxed">
             Piper helps you track and optimise your savings. For every dollar saved you get 10% cashback into your
             account.
           </p>
