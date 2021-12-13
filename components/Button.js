2b808b327abd2bbc6531f6fe83ae8fe4b97852ce
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-13 12:02:44 +1100

    Base theming

--- components/Button.js
+++ components/Button.js
@@ -1,8 +1,11 @@
 const VARIANT_MAP = {
-  bold: 'bg-brand-500 text-white',
-  subtle: 'bg-brand-100 text-black',
-  inverted: 'bg-white text-black',
-  critical: 'bg-critical-500 text-white',
+  bold: 'bg-primary-500 text-white hover:bg-opacity-90 active:bg-opacity-75 focus:ring-2 focus:ring-primary-500 focus:ring-opacity-30 ring-offset-1 ring-offset-transparent outline-none',
+  subtle:
+    'bg-primary-50 text-primary-500 hover:bg-primary-100 active:bg-primary-200 focus:ring-2 focus:ring-primary-500 focus:ring-opacity-30 ring-offset-1 ring-offset-transparent outline-none',
+  inverted:
+    'bg-white text-primary-500 hover:bg-opacity-90 active:bg-opacity-75 focus:ring-2 focus:ring-white focus:ring-opacity-30 ring-offset-1 ring-offset-transparent	outline-none',
+  critical:
+    'bg-red-500 text-white hover:bg-opacity-90 active:bg-opacity-75 focus:ring-2 focus:ring-red-500 focus:ring-opacity-30 ring-offset-1 ring-offset-transparent	outline-none',
 };
 
 export function Button({
@@ -21,7 +24,9 @@ export function Button({
       disabled={disabled}
       className={`px-8 h-12 rounded-lg ${
         block ? 'w-full flex' : 'inline-flex'
-      } items-center justify-center ${variantClasses} ${disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`}
+      } items-center justify-center font-sans font-medium ${variantClasses} ${
+        disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+      }`}
       {...props}
     >
       {loading ? <ButtonSpinner /> : children}
@@ -31,7 +36,7 @@ export function Button({
 
 function ButtonSpinner() {
   return (
-    <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <svg className="animate-spin h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
       <path
         className="opacity-75"
