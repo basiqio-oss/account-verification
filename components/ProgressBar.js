3e0e43fad6e9cd14f81a4fdf89a96d8e2b6d5f08
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-14 17:06:35 +1100

    Added transition class so progress bar animates when progressing, and no border-radius when reaching 100 percent

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,10 +1,11 @@
 export function ProgressBar({ value }) {
   return (
-    // TODO: add transition class to animate
     <div className="relative h-1 sm:h-1.5 md:h-2 w-full">
       <div
         style={{ width: `${value}%` }}
-        className="absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-500 to-primary-accent rounded-r"
+        className={`absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-500 to-primary-accent transition-all ${
+          value === 100 ? 'rounded-none' : 'rounded-r'
+        } `}
       ></div>
     </div>
   );
