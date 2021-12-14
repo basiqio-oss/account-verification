857ac1e1a7fdad7debf6c77dbe0522105dfb68c4
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-14 11:31:58 +1100

    Class name updated due to theme update

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -4,7 +4,7 @@ export function ProgressBar({ value }) {
     <div className="relative h-1 sm:h-1.5 md:h-2 w-full">
       <div
         style={{ width: `${value}%` }}
-        className="absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-500 to-accent rounded-r"
+        className="absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-500 to-primary-accent rounded-r"
       ></div>
     </div>
   );
