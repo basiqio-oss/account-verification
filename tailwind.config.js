0ae2fc3916264431cc169351a89177a363fffc34
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-14 14:07:17 +1100

    Pulling in the default sans font stack as per tailwind documentation, added some more comments on what each of the colours are used for

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
@@ -10,27 +11,27 @@ module.exports = {
       // To create your own palette from a brand colour, you can use a tool like https://www.tailwindshades.com/.
       primary: {
         DEFAULT: '#4737FF',
-        accent: '#9C4EFF', // bg gradients, icons
         50: '#F1F0FF', // subtle button bg,
         100: '#E8E5FF', // subtle button :hover bg,
         200: '#DEDBFF', // subtle button :active bg,
         300: '#9289FF',
         400: '#6D60FF',
-        500: '#4737FF', // bold button bg
+        500: '#4737FF', // bold button bg, primary to accent bg gradient
         600: '#1400FE',
         700: '#1000C6',
         800: '#0B008E',
         900: '#070056',
+        accent: '#9C4EFF', // primary to accent bg gradient
       },
       secondary: {
-        DEFAULT: '#10EDC5', // bg gradients, icons
+        DEFAULT: '#10EDC5',
         50: '#BAFAEF',
         100: '#A7F9EA',
         200: '#81F6E1',
         300: '#5BF4D8',
-        400: '#34F1CF',
-        500: '#10EDC5',
-        600: '#0FD7B3',
+        400: '#34F1CF', // icons on dark bg
+        500: '#10EDC5', // primary to secondary bg gradient
+        600: '#0FD7B3', // icons on light bg
         700: '#0DBF9F',
         800: '#0BA78B',
         900: '#0A8F77',
@@ -47,10 +48,9 @@ module.exports = {
       green: colors.green,
     },
 
-    // TYPEFACE FONT
-    // TODO
+    // FONT
     fontFamily: {
-      sans: ['Inter var', 'sans-serif', 'ui-sans-serif', 'system-ui'],
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
     },
     extend: {},
   },
