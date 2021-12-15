5d0f70b750b0cd47d5f0d1f3a0a682bd18990445
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-15 15:39:59 +1100

    Removed next/image as we wont be using anymore

--- components/AccountVerificationForm/StepLogo.js
+++ components/AccountVerificationForm/StepLogo.js
@@ -1,13 +1,9 @@
-import Image from 'next/image';
-
 // TODO: Should there be a default src?
 export function StepLogo({ src, alt }) {
   return (
     <div className="flex justify-center">
       {/* Square sized svgs recommended (both product logo and institution logo) */}
-      <div className="w-12 h-12 sm:w-16 sm:h-16 relative">
-        <Image src={src} alt={alt} layout="fill" />
-      </div>
+      <img className="w-12 h-12 sm:w-16 sm:h-16 rounded-lg" src={src} alt={alt} />
     </div>
   );
 }
