974069ac2e326ad4fc65acafdaff96afdf9abde1
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-15 11:06:37 +1100

    Added role="list" for screen readers to read it as a list

--- components/AccountVerificationForm/AccountVerificationFormStep1.js
+++ components/AccountVerificationForm/AccountVerificationFormStep1.js
@@ -41,7 +41,7 @@ export function AccountVerificationFormStep1() {
 
         {/* Build trust */}
         {/* Present the user with valid arguments for why it's 100% secure to connect to their bank through the app. */}
-        <ul className="bg-gray-50 rounded-lg border divide-y">
+        <ul role="list" className="bg-gray-50 rounded-lg border divide-y">
           {/* Secure argument 1 */}
           <li className="px-4 sm:px-6 py-3 bg-gradient-to-tr from-primary-500 to-secondary rounded-t-lg space-x-4 flex items-center scale-100">
             <div className="flex flex-grow text-white font-medium leading-snug">
