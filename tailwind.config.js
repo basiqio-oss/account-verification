fa212df9bc29ff29ea5e3cf7ad0dd65b8c8a0f17
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-20 15:36:34 +1100

    Removed some unnecessary copy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     // THEME
-    // To easily see what styles are used where, you can search the codebase for "text-primary-bold" e.g.
+    // To easily see what styles are used where, you can search the codebase for `text-primary-bold` e.g.
     // For more information on how to configure this TailwindCSS theme, go to https://v2.tailwindcss.com/docs/theme
 
     colors: {
@@ -16,24 +16,24 @@ module.exports = {
       // Primary brand colours
       primary: {
         // Subtle
-        subtle: '#F1F0FF', // Used for <Button variant="subtle"/> bg, and radio options e.g.
-        subtledarker: '#E8E5FF', // Used for <Button variant="subtle"/> :hover bg
-        subtledarkest: '#DEDBFF', // Used for <Button variant="subtle"/> :active bg
+        subtle: '#F1F0FF', // <Button variant="subtle"/> bg, and radio options e.g.
+        subtledarker: '#E8E5FF', // <Button variant="subtle"/> :hover bg
+        subtledarkest: '#DEDBFF', // <Button variant="subtle"/> :active bg
 
         // Bold
-        bold: '#4737FF', // Used for <Button variant="bold"/> bg, and primary-bold -> primary-accent bg gradients e.g.
-        bolddarker: '#1400FE', // Used for links (darker to provide more contrast)
+        bold: '#4737FF', // <Button variant="bold"/> bg, and primary-bold -> primary-accent bg gradients e.g.
+        bolddarker: '#1400FE', // Links (darker to provide more contrast)
 
         // Accent
-        accent: '#9C4EFF', // Used for primary-bold to primary-accent" bg gradient e.g.
+        accent: '#9C4EFF', // primary-bold to primary-accent bg gradient e.g.
       },
 
       // Secondary brand colours
       secondary: {
         // Bold
-        boldlighter: '#34F1CF', // Used for icons on dark bg (lighter to provide more contrast)
-        bold: '#10EDC5', // Used for primary-bold to secondary-bold bg gradients e.g.
-        bolddarker: '#0FD7B3', // Used for icons on light bg (darker to provide more contrast)
+        boldlighter: '#34F1CF', // icons on dark bg (lighter to provide more contrast)
+        bold: '#10EDC5', // primary-bold to secondary-bold bg gradients e.g.
+        bolddarker: '#0FD7B3', // icons on light bg (darker to provide more contrast)
       },
 
       // FUNCTIONAL COLOURS
@@ -41,28 +41,28 @@ module.exports = {
 
       // Base UI colours
       transparent: 'transparent',
-      current: 'currentColor', // Used for svgs to be able to grab the text-{color} as stroke- or fill colour
-      black: '#000217', // Used for default text colour
-      white: '#FFFFFF', // Used for default bg colour of the /account-verification flow, <Button variant="inverted"/> e.g.
+      current: 'currentColor', // svgs to be able to grab the text-{color} as stroke- or fill colour
+      black: '#000217', // default text colour
+      white: '#FFFFFF', // default bg colour of the /account-verification flow, <Button variant="inverted"/> e.g.
 
       // Neutral UI colours
       neutral: {
-        subtle: '#F5F8F9', // Used for subtle backgrounds to contrast with default white bg e.g.
-        subtledarker: '#EBEEEF', // Used for <InstitutionsLoadingSkeleton />, disabled bg colour e.g.
-        dim: '#DDE2E4', // User for default border colour
-        dimdarker: '#BECBD0', // Used for input border, radio circles e.g.
-        muted: '#7E888E', // Used for muted icons, e.g. in <SearchInput />
-        muteddarker: '#4F6772', // Used for muted text (darker to provide more contrast)
+        subtle: '#F5F8F9', // subtle backgrounds to contrast with default white bg e.g.
+        subtledarker: '#EBEEEF', // <InstitutionsLoadingSkeleton />, disabled bg colour e.g.
+        dim: '#DEE4E7', // User for default border colour
+        dimdarker: '#BECBD0', // input border, radio circles e.g.
+        muted: '#7E888E', // muted icons, e.g. in <SearchInput />
+        muteddarker: '#4F6772', // muted text (darker to provide more contrast)
       },
 
       // Critical UI colours
       critical: {
         // subtle
-        subtle: '#FFE9EB', // Used for error message backgrounds e.g.
+        subtle: '#FFE9EB', // error message backgrounds e.g.
 
         // bold
-        bold: '#E8001C', // Used for <Button variant="critical"/> bg, error border colour e.g.
-        bolddarker: '#CB0018', // Used for error message text colour (darker to provide more contrast)
+        bold: '#E8001C', // <Button variant="critical"/> bg, error border colour e.g.
+        bolddarker: '#CB0018', // error message text colour (darker to provide more contrast)
       },
     },
 
