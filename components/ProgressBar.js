cd29f45f2289c2d6af77219d79601f64a437ed59
Jordan Overbye <jordanoverbye@gmail.com>
2021-12-24 10:27:37 +1000

    ProgressBar

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -3,7 +3,7 @@ export function ProgressBar({ value }) {
     <div className="relative w-full h-1 bg-white sm:h-1.5 md:h-2">
       <div
         style={{ width: `${value}%` }}
-        className={`absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-bold to-primary-accent transition-all ${
+        className={`absolute top-0 left-0 bottom-0 tracking-tight tabular-nums bg-gradient-to-r from-primary-bold to-primary-accent transition-all ${
           value === 100 ? 'rounded-none' : 'rounded-r'
         } `}
       />
