82a43811308808c6f196b5d41f0b79ec3cfa13b6
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2021-12-29 09:38:08 +1000

    Add PasswordField component (#27)
    * useToggleState
    
    * Add component
    
    * Render password field
    
    * Classname linting
    
    * Style and a11y tweaks
    
    Co-authored-by: Johan Brolin <johan.brolin@thinkmill.com.au>

--- components/AccountVerificationForm/AccountVerificationFormStep3InstitutionLogin.js
+++ components/AccountVerificationForm/AccountVerificationFormStep3InstitutionLogin.js
@@ -4,6 +4,7 @@ import ms from 'ms';
 import { useTernaryState } from '../../utils/useTernaryState';
 import { Button } from '../Button';
 import { TextField } from '../TextField';
+import { PasswordField } from '../PasswordField';
 import { VerificationProgress } from '../VerificationProgress';
 import { ErrorMessage } from '../ErrorMessage';
 import { useAccountVerificationForm } from './AccountVerificationFormProvider';
@@ -26,7 +27,7 @@ function AccountVerificationFormStep3InstitutionLoginForm() {
   const { goBack, accountVerificationFormState, createBasiqConnection } = useAccountVerificationForm();
   const { selectedInstitution } = accountVerificationFormState;
 
-  const [formState, { text, password }] = useFormState();
+  const [formState, { text }] = useFormState();
   const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState();
 
@@ -39,14 +40,17 @@ function AccountVerificationFormStep3InstitutionLoginForm() {
         const label = selectedInstitution[key];
         const placeholder = selectedInstitution[key];
         return {
-          id,
-          label,
-          placeholder,
+          field: {
+            id,
+            label,
+            placeholder,
+            ...text(id),
+          },
           // We should always show a password field, except for the loginId field
-          ...(id === 'loginId' ? text(id) : password(id)),
+          FieldComponent: id === 'loginId' ? TextField : PasswordField,
         };
       });
-  }, [password, selectedInstitution, text]);
+  }, [selectedInstitution, text]);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -92,9 +96,9 @@ function AccountVerificationFormStep3InstitutionLoginForm() {
               {/* Error state */}
               {error && <ErrorMessage message={error.message} />}
 
-              {formFields.map(field => (
+              {formFields.map(({ FieldComponent, field }) => (
                 <div key={field.id} className="space-y-2">
-                  <TextField {...field} disabled={submitting} />
+                  <FieldComponent {...field} disabled={submitting} />
                   {/** Forgot password */}
                   {field.id === 'password' && selectedInstitution.forgottenPasswordUrl && (
                     <a
