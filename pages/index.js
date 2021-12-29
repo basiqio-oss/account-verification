f1ae17710fdc56705c3c4af443a93174f61bc95c
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-30 08:51:03 +1100

    Added rest of blurry circles

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,15 @@ export default function Home() {
     <div>
       <SEO />
       <main className="flex flex-col justify-center min-h-screen bg-gradient-to-tr from-primary-bold to-primary-accent">
-        <div className="absolute -mt-10 bg-white rounded-full opacity-75 w-52 h-52 top-1/2 left-1/2 -ml-80 blur-md mix-blend-overlay" />
-        <div className="absolute w-32 h-32 ml-24 -mt-32 bg-white rounded-full top-1/2 left-1/2 blur-sm mix-blend-overlay opacity-30" />
+        {/* Decorative blurry circles */}
+        <div className="absolute w-full h-screen overflow-hidden">
+          <div className="absolute -mt-64 bg-white rounded-full opacity-40 w-80 h-80 top-1/2 left-1/2 -ml-[800px] blur-xl mix-blend-overlay" />
+          <div className="absolute -mt-10 bg-white rounded-full opacity-75 w-52 h-52 top-1/2 left-1/2 -ml-80 blur-md mix-blend-overlay" />
+          <div className="absolute -ml-16 bg-white rounded-full -mt-[550px] opacity-10 w-72 h-72 top-1/2 left-1/2 blur-2xl mix-blend-overlay" />
+          <div className="absolute w-32 h-32 ml-24 -mt-32 bg-white rounded-full opacity-25 top-1/2 left-1/2 blur-sm mix-blend-overlay" />
+          <div className="absolute mt-24 bg-white rounded-full opacity-50 w-96 h-96 top-1/2 left-1/2 ml-[450px] blur-xl mix-blend-overlay" />
+        </div>
+
         <div className="z-10 max-w-md px-4 pt-8 mx-auto text-center pb-14 space-y-6">
           {/* Product logo and divider */}
           <div className="inline-block space-y-6">
