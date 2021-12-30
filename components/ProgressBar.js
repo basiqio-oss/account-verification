8ef2c05d5d2bd1c87730fbf60f4dfb040a7792ff
Johan Brolin <johan.brolin@thinkmill.com.au>
2021-12-31 07:05:42 +1100

    Tweaked size of ProgressBar to stay the same

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,6 +1,6 @@
 export function ProgressBar({ value }) {
   return (
-    <div className="relative w-full bg-white h-1.5 md:h-2">
+    <div className="relative w-full bg-white h-1.5">
       <div
         style={{ width: `${value}%` }}
         className={`absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-bold to-primary-accent transition-all ${
