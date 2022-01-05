455bc975c3eb5e0693d28a691dd5fa07ad21632e
Bridget Turnbull <bridget@192-168-1-108.tpgi.com.au>
2022-01-05 12:14:33 +1100

    Update copy

--- components/AccountVerificationForm/AccountVerificationFormStep3InstitutionLogin.js
+++ components/AccountVerificationForm/AccountVerificationFormStep3InstitutionLogin.js
@@ -171,7 +171,7 @@ function AccountVerificationFormStep3InstitutionLoginProgress() {
               <h2 className="text-xl font-semibold tracking-tight sm:text-2xl">{STEP_NAME_MAP[stepNameInProgress]}</h2>
               <p className="text-sm sm:text-base text-neutral-muted-darker">
                 {estimatedTimeOver
-                  ? `It\u2019 taking a bit longer than expected`
+                  ? `It\u2019s taking a bit longer than expected`
                   : `Usually takes ${ms(estimatedTime, { long: true })}`}
               </p>
             </div>
