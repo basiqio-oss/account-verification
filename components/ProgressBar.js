20ddde8b0dd8f5f9d9b453f3f16c852a11fda0f9
Johan Brolin <johan.brolin@thinkmill.com.au>
2022-01-07 09:41:34 +1100

    Flipped the gradient of ProgressBar

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -3,7 +3,7 @@ export function ProgressBar({ value }) {
     <div className="relative w-full bg-white h-1.5">
       <div
         style={{ width: `${value}%` }}
-        className={`absolute top-0 left-0 bottom-0 bg-gradient-to-r from-primary-bold to-primary-accent transition-all ${
+        className={`absolute top-0 left-0 bottom-0 bg-gradient-to-r to-primary-bold from-primary-accent transition-all ${
           value === 100 ? 'rounded-none' : 'rounded-r'
         } `}
       />
