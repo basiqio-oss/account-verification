dc204db7d5af34dc6317e58407c5f9366e2a0271
Johan Brolin <johan.brolin@thinkmill.com.au>
2022-01-06 11:36:44 +1100

    Tweaked style of inverted button to use secondary brand colour for background

--- components/Button.js
+++ components/Button.js
@@ -6,7 +6,7 @@ const VARIANT_MAP = {
   subtle:
     'bg-primary-subtle text-primary-bold hover:bg-primary-subtle-darker active:bg-primary-subtle-darkest focus:ring-2 focus:ring-primary-bold focus:ring-opacity-30 ring-offset-1 ring-offset-transparent outline-none',
   inverted:
-    'bg-white text-primary-bold hover:bg-opacity-90 active:bg-opacity-75 focus:ring-2 focus:ring-white focus:ring-opacity-30 ring-offset-1 ring-offset-transparent outline-none',
+    'bg-secondary-bold text-primary-bold-darker hover:bg-opacity-90 active:bg-opacity-75 focus:ring-2 focus:ring-white focus:ring-opacity-30 ring-offset-1 ring-offset-transparent outline-none',
   critical:
     'bg-critical-bold text-white hover:bg-opacity-90 active:bg-opacity-75 focus:ring-2 focus:ring-critical-bold focus:ring-opacity-30 ring-offset-1 ring-offset-transparent	outline-none',
 };
