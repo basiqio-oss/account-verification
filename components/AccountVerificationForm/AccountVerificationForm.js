21e236d5da6c2dd50c3b26000a4cac5a94449a78
Johan Brolin <johan.brolin@thinkmill.com.au>
2022-01-07 09:54:50 +1100

    Tweaked AV background illustration colours a bit more for aesthetic balance

--- components/AccountVerificationForm/AccountVerificationForm.js
+++ components/AccountVerificationForm/AccountVerificationForm.js
@@ -89,7 +89,7 @@ export function AccountVerificationForm() {
         <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1322 229">
           <path
             d="M1.86 188.088a11.025 11.025 0 0 0 10.834-1.95c3.794-3.256 4.984-8.62 5.952-13.587l2.864-14.69-5.995 4.22c-4.311 3.034-8.72 6.166-11.704 10.561-2.985 4.395-4.288 10.394-1.89 15.15"
-            className="text-primary-subtle"
+            className="text-secondary-bold"
             fill="currentColor"
           />
           <path
@@ -99,7 +99,7 @@ export function AccountVerificationForm() {
           />
           <path
             d="M8.216 197.88a8.205 8.205 0 0 0 7.196 3.796c3.643-.177 6.68-2.775 9.414-5.243l8.086-7.298-5.352-.262c-3.848-.188-7.796-.363-11.462.85-3.666 1.213-7.046 4.133-7.717 8.011"
-            className="text-primary-subtle"
+            className="text-secondary-bold"
             fill="currentColor"
           />
           <path
@@ -109,12 +109,12 @@ export function AccountVerificationForm() {
           />
           <path
             d="M135.803 50.594a5.966 5.966 0 0 1-2.642 7.904 5.73 5.73 0 0 1-.882.348l-.988 20.843-8.563-6.401 2.251-18.812a5.943 5.943 0 0 1 3.091-6.584 5.72 5.72 0 0 1 7.733 2.702Z"
-            className="text-neutral-dim-darker"
+            className="text-primary-subtle-darkest"
             fill="currentColor"
           />
           <path
             d="m148.318 200.769 5.536-1.118-1.584-22.357-8.17 1.65 4.218 21.825Z"
-            className="text-neutral-dim"
+            className="text-primary-subtle-darkest"
             fill="currentColor"
           />
           <path
@@ -124,7 +124,7 @@ export function AccountVerificationForm() {
           />
           <path
             d="M128.911 204.045h5.643l2.684-22.248h-8.328l.001 22.248Z"
-            className="text-neutral-dim"
+            className="text-primary-subtle-darkest"
             fill="currentColor"
           />
           <path
@@ -159,17 +159,17 @@ export function AccountVerificationForm() {
           />
           <path
             d="m137.943 194.89-11.087 1.03-6.048-44.819c-19.936 3.428-38.743 4.526-55.01.379a10.516 10.516 0 0 1-7.52-12.701c.064-.263.137-.524.22-.781 1.947-5.94 5.377-12.305 9.392-18.837l26.71 9.788 1.009 4.121 28.49-3.686a10.502 10.502 0 0 1 10.436 5.338 10.504 10.504 0 0 1 1.252 3.924c.038.309.063.62.075.931l2.081 55.313Z"
-            className="text-primary-subtle-darker"
+            className="text-secondary-bold-darker"
             fill="currentColor"
           />
           <path
             d="m154.8 185.452-11.088 1.03-20.103-41.112c-19.936 3.429-38.743 4.527-55.01.38a10.514 10.514 0 0 1-7.52-12.702c.063-.263.136-.523.22-.781 1.947-5.939 5.377-12.304 9.392-18.837l30.994 2.608-3.275 11.302 28.49-3.687a10.52 10.52 0 0 1 7.773 2.198 10.504 10.504 0 0 1 3.915 7.064c.038.31.063.62.075.932l16.137 51.605Z"
-            className="text-primary-subtle"
+            className="text-secondary-bold"
             fill="currentColor"
           />
           <path
             d="M102.558 53.547c7.515 0 13.607-6.228 13.607-13.91s-6.092-13.91-13.607-13.91c-7.516 0-13.608 6.228-13.608 13.91s6.092 13.91 13.608 13.91Z"
-            className="text-neutral-dim"
+            className="text-primary-subtle-darkest"
             fill="currentColor"
           />
           <path
@@ -195,7 +195,7 @@ export function AccountVerificationForm() {
           />
           <path
             d="M123.238 54.846a5.982 5.982 0 0 0 2.02 8.094c.272.163.557.302.852.418l-.634 20.857 9.033-5.686-.783-18.934a5.965 5.965 0 0 0-2.569-6.814 5.71 5.71 0 0 0-7.919 2.065Z"
-            className="text-neutral-dim"
+            className="text-primary-subtle-darkest"
             fill="currentColor"
           />
           <path
@@ -215,18 +215,18 @@ export function AccountVerificationForm() {
           />
           <path
             d="M114.391 23.875c-2.907-1.71-6.446-1.715-9.8-1.672l-7.291.093c-2.95.037-6.064.121-8.577 1.7-2.513 1.58-4.036 5.221-2.377 7.715-4.934-4.383-12.324-5.075-18.51-2.913-3.027 1.058-5.891 2.775-7.947 5.28-2.233 2.722-3.396 6.226-3.863 9.746-.468 3.52-.285 7.093-.102 10.64.115 2.23.368 4.746 2.085 6.127 1.584 1.275 3.906 1.082 5.745.24 3.208-1.469 5.426-4.547 7.084-7.716 1.658-3.168 2.922-6.575 4.986-9.483 2.064-2.909 5.179-5.341 8.694-5.383-2.39 3.466-2.827 8.505-.329 11.89 1.797 2.436 4.794 3.7 7.761 4.02 2.967.322 5.953-.188 8.895-.697 1.992-.344 4.334-.977 4.993-2.929.647-1.914-.739-3.847-1.332-5.779-1.145-3.729 1.203-8.185 4.879-9.26l-.299-.296c3.043.434 6.689.032 8.309-2.638 1.769-2.913-.096-6.975-3.004-8.685Z"
-            className="text-neutral-dim-darker"
+            className="text-primary-bold"
             fill="currentColor"
           />
           <g clipPath="url(#a)">
             <path
               d="M1201.13 139.042c0-9.914-8.07-33.442-18.03-33.442-9.95 0-18.03 23.528-18.03 33.442 0 4.765 1.9 9.334 5.28 12.703a18.084 18.084 0 0 0 12.75 5.262c4.78 0 9.37-1.893 12.75-5.262a17.936 17.936 0 0 0 5.28-12.703Z"
-              className="text-primary-subtle-darker"
+              className="text-secondary-bold-darker"
               fill="currentColor"
             />
             <path
               d="M1196.62 139.042c0-9.914-8.07-33.442-18.03-33.442-9.95 0-18.02 23.528-18.02 33.442 0 4.765 1.9 9.334 5.28 12.703a18.043 18.043 0 0 0 12.74 5.262c4.79 0 9.37-1.893 12.75-5.262a17.936 17.936 0 0 0 5.28-12.703Z"
-              className="text-primary-subtle"
+              className="text-secondary-bold"
               fill="currentColor"
             />
             <path
@@ -256,12 +256,12 @@ export function AccountVerificationForm() {
             />
             <path
               d="M1321.2 160.994c0-12.035-9.88-40.594-22.06-40.594-12.18 0-22.05 28.559-22.05 40.594 0 5.783 2.32 11.33 6.46 15.419 4.13 4.089 9.74 6.387 15.59 6.387s11.46-2.298 15.6-6.387c4.13-4.089 6.46-9.636 6.46-15.419Z"
-              className="text-primary-subtle-darker"
+              className="text-secondary-bold-darker"
               fill="currentColor"
             />
             <path
               d="M1316.39 160.994c0-12.035-9.88-40.594-22.06-40.594-12.18 0-22.05 28.559-22.05 40.594 0 5.783 2.32 11.33 6.46 15.419 4.13 4.089 9.74 6.387 15.59 6.387s11.46-2.298 15.6-6.387c4.13-4.089 6.46-9.636 6.46-15.419Z"
-              className="text-primary-subtle"
+              className="text-secondary-bold"
               fill="currentColor"
             />
             <path
